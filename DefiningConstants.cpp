dccbe09cce2d08477cce707bd8fe8222d21af60b
choi <gracedays@naver.com>
2020-11-12 09:35:22 +0900

    modified

--- DefiningConstants.cpp
+++ DefiningConstants.cpp
@@ -1,15 +1,15 @@
 // # define indentifier value      : 상수선언 방법 1
 // const type variable = value;    : 상수선언 방법 2
 
-#include <iostream>  
+# include <iostream>  
 using namespace std;
 
 # define LENGTH 10       // 상수선언 방법 1
 # define NEWLINE '\n'
 int main(){
     const int WIDTH = 5; // 상수선언 방법 2
-    // LENGTH = 20;
-    // WIDTH = 10;
+    // LENGTH = 20;      // 상수 선언 했으므로 에러  --> 주석처리
+    // WIDTH = 10;       // 상수 선언 했으므로 에러  --> 주석처리 
     int area;
     area = LENGTH * WIDTH;
     cout << area;
