8f262b6500c26a36d61c69a1f43668fdb035f03f
choi <gracedays@naver.com>
2020-11-13 11:49:01 +0900

    annotation added

--- NameSpaces-1.cpp
+++ NameSpaces-1.cpp
@@ -1,9 +1,32 @@
 #include"under/namespaceOther.cpp"
-// using namespace first_space;
-// using namespace second_space;
+using namespace std;
+using namespace first_space;
 
 int main(){                           // in the case of the same function names, 
-    first_space::func();                           // inside first_space   because of "using namespace first_space"
+    func();                           // inside first_space   because of "using namespace first_space"
     second_space::func();             // inside second_space , specified by :: (scope resolution)
     return 0;
 }
+
+/*
+[ namespace 참조 할 때 ]
+
+방법 1.
+#include"under/namespaceOther.cpp"
+using namespace std; 
+using namespace first_space;
+
+int main(){
+    func();
+    second_space::func();
+    return 0;
+}
+
+방법 2.
+#include"under/namespaceOther.cpp"
+int main(){
+    first_space::func();
+    second_space::func();
+    return 0;
+}
+*/
