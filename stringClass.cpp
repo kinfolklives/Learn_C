452e9fd45e4ca7e53a797841e2ef3260e1e2b804
choi <gracedays@naver.com>
2020-11-16 11:39:08 +0900

    string class modified

--- stringClass.cpp
+++ stringClass.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 #include<string>
 int main(){
-    string str1 = "Hello", str2 = "World", str3;
+    string str1 = "Hello", str2 = "World" , str3;
+    // int i = 3 
     int len;
     str3 = str1;   // copy str1 into str3
     cout << "str3: " << str3 << endl;
     // str3 : Hello
-    str3 = str1 + str2;
+    // str3 = str1 + i;    // 타입이 다르면 concatenation 불가 
+    str3 = str1 + str2 ;
     cout << "str1 + str2 : " << str3 << endl;
     // str1 + str2 : HelloWorld
     len = str3.size();  // total length of str3 after concatenation
