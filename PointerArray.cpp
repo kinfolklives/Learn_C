7ba6c87d4a89db7dea2679b22569bc7e07f36062
choi <gracedays@naver.com>
2020-11-17 11:57:54 +0900

    pointer array

--- PointerArray.cpp
+++ PointerArray.cpp
@@ -6,15 +6,20 @@ using namespace std;
 int main(){
     int var[MAX] = {10,100,200}, *ptr_first = var;
     for(int i = 0; i<MAX; i++){
+
         cout << "Array var["<<i<<"] = " << var[i] << ", address: " << &var[i] << endl;
         cout << "Pointer var["<<i<<"] = "<< *ptr_first <<", addr: " << ptr_first << endl;
-        // ++ptr_first;
+        ++ptr_first;
     } // method 1
+
+    cout << "--------------------------------" << endl;
 
     int *ptr_second;
     for(int i = MAX-1; i>=0; i--){
         ptr_second = &var[i];
-        cout << "..." << *ptr_second <<", addr: "<<ptr_second <<endl;
+        
+        cout << "Array var["<<i<<"] = " << var[i] << ", address: " << &var[i] << endl;
+        cout << "Pointer var["<<i<<"] = " << *ptr_second <<", addr: "<< ptr_second <<endl;
     } // method 2
 
     return 0;
